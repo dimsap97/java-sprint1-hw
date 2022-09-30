55daa316107c05e1dc2ae3ba96c8d6eb23af4f8a
dimsap97 <dimsap97@yandex.ru>
2022-09-30 18:31:36 +0300

    Почти работает

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,102 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        StepTracker tracker = new StepTracker();
+
+        while (true) /*Реализуем бесконечный цикл для удобного меню*/ {
+            printMenu();
+            Scanner scanner = new Scanner(System.in);
+            int command = scanner.nextInt();
+            if (command == 0) {
+                break;
+            } else if(command == 3) {
+              System.out.println("Какой цели вы желаете достичь?");
+                int newGoal = scanner.nextInt();
+                tracker.changeGoal(newGoal);
+            } else if (command == 2) {
+                   System.out.println("За какой месяц вы желаете просмотреть активность?");
+                   int month = scanner.nextInt();
+                   tracker.printStatsForMonth(month);
+        } else if (command == 4) {
+                rememberSteps(scanner, tracker);
+            } else if (command == 1) {
+
+
+                    printResultsForDay(scanner, tracker);
+
+            }
+
+        }
     }
+ public static void printMenu() {
+     System.out.println("Приветствуем в нашем календаре активности.");
+     System.out.println("Здесь вы можете следить за качеством и количеством своих прогулок в течение года");
+     System.out.println("Наш календарь умеет:");
+     System.out.println("1 - Выводить число шагов за день");
+     System.out.println("2 - Приводить статисткику прогулок в течение выбранного месяца");
+     System.out.println("3 - Менять желаемое количесвто шагов в день");
+     System.out.println("4 - Запоминать число пройденных за день шагов");
+     System.out.println("В случае, если календарь вам надоест, достаточно ввести 0 и выйти из программы");
+ }
+ public static void printResultsForDay(Scanner scanner, StepTracker tracker) {
+     System.out.println("Укажите месяц. Для выхода введите 0.");
+     int month = scanner.nextInt();
+    while(true) {
+     if (month < 0) {
+         System.out.println("Такого месяца не существует!");
+     } else if (month > 12) {
+         System.out.println("Такого месяца не существует!");
+     } else if (month == 0) {
+         break;
+     } else {
+         while (true) {
+             System.out.println("За какой день вы хотите увидеть свою активность? Для выхода введите 0.");
+             int day = scanner.nextInt();
+             if (day == 0) {
+                 break;
+             } else if (day > 30) {
+                 System.out.println("Такого дня не существует!");
+             } else if (day < 0) {
+                 System.out.println("Такого дня не существует!");
+             } else {
+                 tracker.printStatsForDay(day, month);
+             }
+         }
+     }
+ }
+ }
+ public static void rememberSteps (Scanner scanner, StepTracker tracker) {
+     System.out.println("Обнулить значения активностей? Да - 0, любое другое значение - нет");
+     System.out.println("(В случае, если вы ещё не заполняли активности за год, мы рекоммендуем вам обнулить их значения.)");
+     int zero = scanner.nextInt();
+     if (zero == 0) {
+         tracker.resetActivity();
+     } else {
+         while (true) {
+             System.out.println("Введите, пожалуйста, номер месяца, за который вы хотите изменить активность.");
+             System.out.println("Чтобы вернуться в предыдущее меню, ввекдите 0.");
+             int month = scanner.nextInt();
+             if (month == 0) {
+                 break;
+             } else if (month < 0) {
+                 System.out.println("Номер месяца не может выпадать из отрезка 1-12 :-(");
+             } else if (month > 12) {
+                 System.out.println("Номер месяца не может выпадать из отрезка 1-12 :-(");
+             } else {
+                 System.out.println("За какой день вы желаете изменить активность? Для выхода из меню введите 0.");
+                 int day = scanner.nextInt();
+                 if (day < 0) {
+                     System.out.println("Значение дня не может быть отрицательным!");
+                 } else if (day > 30) {
+                     System.out.println("В нашем юном приложении в месяце не более 30 дней :-(");
+                 } else {
+                     tracker.writeDownSteps(month, day);
+                 }
+             }
+
+         }
+     }
+ }
+
 }
---
+++ src/StepTracker.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class StepTracker {
+    int steps[][] = new int[12][30];
+    int theStartGoal = 10000;
+
+    void changeGoal(int newGoal) {
+
+        theStartGoal = newGoal;
+    }
+    void writeDownSteps(int month, int day) {
+        Scanner scanner = new Scanner(System.in);
+                        System.out.println("Сколько вы прошли за этот день?");
+                        steps[month - 1][day - 1] = scanner.nextInt();
+            }
+    void resetActivity() {
+
+        for(int i = 0; i < 12; i++ ) {
+            for(int j = 0; j < 30; j++) {
+                steps[i][j] = 0;
+            }
+        }
+    }
+
+    void printStatsForDay(int month, int day) {
+
+        System.out.println("Вы прошли " + steps[month - 1][day - 1] + " шагов");
+    }
+    void printStatsForMonth(int month) {
+        int max = 0;
+      for (int i = 0; i < 30; i++) {
+          System.out.println((i + 1) + " день: " + steps[month - 1][i] + ",");
+          } int sum = 0;
+      for (int i = 0; i < 30; i++) {
+        sum = sum + steps[month - 1][i];
+      } System.out.println("Всего за месяц пройдено: " + sum);
+      System.out.println("Среднее число шагов в день: " + (sum/30));
+      for (int i = 0; i <30; i++) {
+          if (steps[month - 1][i] > max) {
+             max = steps[month - 1][i];
+          }
+      }
+      System.out.println("Ваш рекорд за месяц - " + max + " шагов.");
+      int score1 = 0;
+      int maxScore = 0;
+        for (int i = 0; i < 30; i++) {
+
+                if (steps[month][i] >= theStartGoal) {
+                    score1 = score1 + 1;
+                    if (score1 > maxScore) {
+                        maxScore = score1;
+                    }
+                } else {
+                    score1 = 0;
+                }
+
+                    }
+
+        System.out.println("Лучшая серия: " + maxScore + " дней подряд.");
+    }
+
+        }
+
+
+
+
